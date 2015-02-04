60a2a08905792fdb2c5f003b542b2f3c7cfa42e2
Dan Jones <dan.jones@moneydesktop.com>
2015-02-03 19:49:52 -0700

    Migrate for DailyReport

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203165022) do
+ActiveRecord::Schema.define(version: 20150204024438) do
+
+  create_table "daily_reports", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "objectives", force: true do |t|
     t.datetime "created_at"
